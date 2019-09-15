d44495d25a4463002f7c6c6eac8e454bc9aeff30
Katrina Brinatte <up5757oo@metrostate.edu>
2019-09-15 14:55:10 -0500

    Update scanner_releases_gantt.php

--- scanner_releases_gantt.php
+++ scanner_releases_gantt.php
@@ -24,24 +24,29 @@
       var data = new google.visualization.DataTable();
       data.addColumn('string', 'ID');
       data.addColumn('string', 'Name');
-      data.addColumn('string', 'App ID');
+      data.addColumn('string', 'ID');
       data.addColumn('date', 'Start Date');
       data.addColumn('date', 'End Date');
       data.addColumn('number', 'Duration');
       data.addColumn('number', 'Percent Complete');
-      data.addColumn('string', 'Dependantcies');
+      data.addColumn('string', 'Dependencies');
   data.addRows([
     
    <?php 
-        $sql = "SELECT id, name, app_id, DATE_FORMAT(open_date, '%Y, %m, %e') as open_date, DATE_FORMAT(rtm_date, '%Y, %m, %e') as rtm_date, (rtm_date - open_date) as duration, 0 as percent_complete, 'null' as dependantcies FROM releases where rtm_date > open_date  and rtm_date > sysdate() order by app_id, rtm_date;";
+        $sql = "SELECT distinct id, name, DATE_FORMAT(dependency_date, '%Y, %m, %e') as dependency_date, DATE_FORMAT(open_date, '%Y, %m, %e') as open_date, DATE_FORMAT(rtm_date, '%Y, %m, %e') as rtm_date, (rtm_date - open_date) as duration, 0 as percent_complete, 'null' as dependencies,  type, status, manager, author FROM releases /* where rtm_date > open_date  and rtm_date > sysdate()*/ order by open_date, dependency_date, rtm_date;";
         $result = $db->query($sql);
-        
                         if ($result->num_rows > 0) {
                             // output data of each row
                             while($row = $result->fetch_assoc()) {
-                                echo '["'.$row["id"].'", "'.$row["name"].'", "'.$row["app_id"].
-                                '", new Date('.$row["open_date"].'), new Date('.$row["rtm_date"].') , '.$row["duration"].', '
-                                .$row["percent_complete"].', '.$row["dependantcies"].'],';
+                              $start_date = $row["open_date"];
+                              $type = $row["type"];
+                              $status = $row["status"];
+                              $manager = $row["manager"];
+                              $author = $row["author"];
+
+                                echo '["'.$row["id"].'", "'.$row["name"].'", "Type: '.$type.
+                                ' | Status: '.$status.' | Manager: '.$manager.' | Author: '.$author.'", new Date('.$start_date.'), new Date('.$row["rtm_date"].') , '.$row["duration"].', '
+                                .$row["percent_complete"].', '.$row["dependencies"].'],';
                           
                                 
                             }
@@ -57,9 +62,10 @@
 
   ]);
       var options = {
-        height: 400,
+        height: 600,
         gantt: {
-          trackHeight: 30
+          trackHeight: 30,
+          criticalPathEnabled: false
         }
       };
 
