473fe5da592f2ade67251020f9a9491da2c5a89b
Katrina Brinatte <up5757oo@metrostate.edu>
2019-09-16 06:47:02 -0500

    Update scanner_releases_gantt.php

--- scanner_releases_gantt.php
+++ scanner_releases_gantt.php
@@ -8,18 +8,17 @@
   global $db;
 
   ?>
-
-
 <div class="right-content">
     <div class="container">
 
       <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
-
+<!--Enabling the script options necessary for builing the Google Gantt chart-->
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script type="text/javascript">
     google.charts.load('current', {'packages':['gantt']});
     google.charts.setOnLoadCallback(drawChart);
 
+    //Builing the function that will create the Gantt chart
     function drawChart() {
       var data = new google.visualization.DataTable();
       data.addColumn('string', 'ID');
@@ -30,57 +29,67 @@
       data.addColumn('number', 'Duration');
       data.addColumn('number', 'Percent Complete');
       data.addColumn('string', 'Dependencies');
-  data.addRows([
-    
-   <?php 
-        $sql = "SELECT distinct id, name, DATE_FORMAT(dependency_date, '%Y, %m, %e') as dependency_date, DATE_FORMAT(open_date, '%Y, %m, %e') as open_date, DATE_FORMAT(rtm_date, '%Y, %m, %e') as rtm_date, (rtm_date - open_date) as duration, 0 as percent_complete, 'null' as dependencies,  type, status, manager, author FROM releases /* where rtm_date > open_date  and rtm_date > sysdate()*/ order by open_date, dependency_date, rtm_date;";
+  
+      data.addRows([
+        <?php 
+        //sql to pull data form database including dates formated to match the formate to add them into the Gantt chart
+        $sql = "SELECT distinct id, name, DATE_FORMAT(dependency_date, '%Y, %m, %e') as dependency_date, 
+                DATE_FORMAT(open_date, '%Y, %m, %e') as open_date, DATE_FORMAT(rtm_date, '%Y, %m, %e') as rtm_date, 
+                (rtm_date - open_date) as duration, 'null' as dependencies,  type, status, manager, author 
+                FROM releases /*where rtm_date > open_date  and rtm_date > sysdate()*/
+                order by open_date, dependency_date, rtm_date;";
+
+        //using the $sql query to pull database data necessary for Gantt chart
         $result = $db->query($sql);
-                        if ($result->num_rows > 0) {
-                            // output data of each row
-                            while($row = $result->fetch_assoc()) {
-                              $start_date = $row["open_date"];
-                              $type = $row["type"];
-                              $status = $row["status"];
-                              $manager = $row["manager"];
-                              $author = $row["author"];
-
-                                echo '["'.$row["id"].'", "'.$row["name"].'", "Type: '.$type.
-                                ' | Status: '.$status.' | Manager: '.$manager.' | Author: '.$author.'", new Date('.$start_date.'), new Date('.$row["rtm_date"].') , '.$row["duration"].', '
-                                .$row["percent_complete"].', '.$row["dependencies"].'],';
-                          
-                                
-                            }
-                            
-                            //end while
-                        }//end if
-                        else {
-                            echo "['No Date', 'No Data Found', null, null, null, 1 , null, null]";
-                        }//end else
         
-                         $result->close();
-                        ?>
+        if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+            //applying start date to a variable where either open date or dependancy date could be used
+            $start_date = $row["open_date"];
+            //$start_date = $row["dependency_date"];
 
-  ]);
-      var options = {
-        height: 600,
-        gantt: {
-          trackHeight: 30,
-          criticalPathEnabled: false
+            //hard docing percent value to 0
+            $percent = 0 ;
+            
+            //applying the database values into the 8 columns necessary for generating a Gantt chart  
+            echo '["'.$row["id"].'", "'.$row["name"].'", "Type: '.$row["type"].
+              ' | Status: '.$row["status"].' | Manager: '.$row["manager"].' | Author: '.$row["author"].'", 
+              new Date('.$start_date.'), new Date('.$row["rtm_date"].') , '
+              .$row["duration"].', '.$percent.', '.$row["dependencies"].'],';
+          }//end while
+        }//end if
+        else {
+          //hard coding a response if data is not found from the sql response
+          echo "['No Date', 'No Data Found', null, new Date(2019, 01, 01), new Date(2019, 01, 02), 1 , 0, null]";
+        }//end else
+        //Closing the database results
+        $result->close();
+        ?>
+        ]);
+        //setting the variable options for the Gantt chart
+        var options = {
+          //limitting the height to 600 pixels
+          height: 600,
+          gantt: {
+            //limiting the track height to 30 pixels
+            trackHeight: 30,
+            //disabling the Critical path from appearing in the tool tips
+            criticalPathEnabled: false
+          }
+        };
+        //creating the Gantt chart object
+        var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+      </script>
+      <!--Displaying the Gantt chart within the Div-->
+      <div id="chart_div"></div>
+      <div>
+      <style>
+      tfoot {
+        display: table-header-group;
         }
-      };
-
-      var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
-  </script>
-    <div id="chart_div"></div>
-
-<div>
-   <style>
-   tfoot {
-     display: table-header-group;
-   }
- </style>
-</div>
-  <?php include("./footer.php"); ?>
+        </style>
+        </div>
+        <?php include("./footer.php"); ?>
